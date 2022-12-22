7cc1ff3ecdc9b16d38bcb0f0170ae54961fc0a4d
Valentin Korneev <v.korneef@gmail.com>
2022-12-22 20:16:43 +0600

    add BrowserRouter wrapper for App in test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,14 @@
 import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 //This test does nothing
 test('renders learn react link', () => {
-  render(<App />);
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
   const linkElement = 1
   expect(linkElement).toEqual(1);
 });
